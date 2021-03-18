f774ccb99d235389c7cd5cc2a9d95742ba6cebf7
Sandamith <80910570+Sandamith@users.noreply.github.com>
2021-03-18 22:14:43 +0530

    Add files via upload

---
+++ error1_send_mail.php
@@ -0,0 +1,41 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require_once __DIR__ . '/vendor/phpmailer/src/Exception.php';
+require_once __DIR__ . '/vendor/phpmailer/src/PHPMailer.php';
+require_once __DIR__ . '/vendor/phpmailer/src/SMTP.php';
+
+// passing true in constructor enables exceptions in PHPMailer
+$mail = new PHPMailer(true);
+
+try {
+    // Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER; // for detailed debug output
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+
+    $mail->Username = 'sandamithwanigasuriya@gmail.com'; // YOUR gmail email
+    $mail->Password = 'BreakingBad404'; // YOUR gmail password
+
+    // Sender and recipient settings
+    $mail->setFrom('example@gmail.com', 'Sandamith');
+    $mail->addAddress('sachinthyasandamith@gmail.com', 'Application-Support');
+    $mail->addReplyTo('example@gmail.com', 'Sender Name'); // to set the reply to
+
+    // Setting the email content
+    $mail->IsHTML(true);
+    $mail->Subject = "Script Execution Error";
+    $mail->Body = 'An error occured during the execution of script. Please do the needful.';
+    $mail->AltBody = 'Plain text message body for non-HTML email client. Gmail SMTP email body.';
+
+    $mail->send();
+    echo "Something went wrong. Email message sent to the Application Support Team.";
+} catch (Exception $e) {
+    echo "Error in sending email. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
